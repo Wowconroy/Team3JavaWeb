db67c5629f3182a4660d18601db21133a527309d
Oleksii <shostak.ok@gmail.com>
2022-02-07 22:11:26 +0200

    Add Session Factory

--- src/main/java/org/teamwork/dao/UserDao.java
+++ src/main/java/org/teamwork/dao/UserDao.java
@@ -1,11 +1,22 @@
 package org.teamwork.dao;
 
+import org.hibernate.SessionFactory;
 import org.teamwork.model.User;
 
 import java.util.List;
 import java.util.Optional;
 
 public class UserDao implements Dao<User>{
+    private SessionFactory sessionFactory;
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public Optional<User> getById(int id) {
         return Optional.empty();
