f0f91f5b53b82f4a3a384266361d11450c3ac422
Oleksii <shostak.ok@gmail.com>
2022-02-09 22:10:05 +0200

    Update Lesha

--- src/main/java/org/MainTrial.java
+++ src/main/java/org/MainTrial.java
@@ -1,9 +1,5 @@
 package org;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.teamwork.dao.UserDao;
-
 public class MainTrial {
     public static void main(String[] args) {
 
