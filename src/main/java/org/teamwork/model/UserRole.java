254dcd5a7f455dff3f7755946ea261a8d1348ab5
Oleksii <shostak.ok@gmail.com>
2022-02-11 21:20:30 +0200

    Update Lesha

--- src/main/java/org/teamwork/model/UserRole.java
+++ src/main/java/org/teamwork/model/UserRole.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class UserRole {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "role_id", nullable = false)
     private Long id;
 
